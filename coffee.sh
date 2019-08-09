e180a26da54630f6e6f53d31e49f32e6e65eabc1
Digvijay Gunjal <digvijag@thoughtworks.com>
2019-08-09 12:31:25 +0530

    Digvijay | coffee - do git stash and then git pull --rebase  through all repos in given directory.

---
+++ coffee.sh
@@ -0,0 +1,41 @@
+#!/bin/sh -x
+
+# Declare colors
+ERROR='\033[0;31m'
+NC='\033[0m' # No Color
+SUCCESS='\033[032m'
+WARNING='\033[033m'
+
+declare -a repos=($(ls -d */))
+cwd=$(pwd)
+commands=("stash" "pull --rebase")
+
+run_git() {
+  printf "[${i}] ${1}\r"
+  OUTPUT=$(git ${1})
+  is_success=$?
+  if [ $is_success -lt 1 ]; then
+    printf "${SUCCESS}git ${command} in ${i} - Successful${NC}\n"
+  else
+    printf "${ERROR}git ${command} in ${i} - FAILED${NC}\n"
+  fi;
+  printf "${OUTPUT}\n\n"
+}
+
+for command in "${commands[@]}"
+do
+  printf "\n--- Running \"git ${command}\" for all repos ---\n\n"
+  for i in "${repos[@]}"
+  do
+    if [ -d "$cwd/$i" ]; then
+      cd $cwd"/"$i
+      if [ -d .git ]; then
+        run_git $command
+      else
+        printf "${WARNING}[Ignored] \"git ${command}\". Not a git repo \"${i}\"${NC}\n\n"
+      fi;
+    else
+      printf "${WARNING}[Ignored] ${i} is not a directory${NC}\n\n"
+    fi
+  done
+done
